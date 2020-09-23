414814bfaf7680c1c43babbc82c2016dc2350219
vinay.garimella <vg5ak@virginia.edu>
2020-09-23 18:57:31 -0400

    YE

--- microdollars/models.py
+++ microdollars/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+"This is a new comment"
+class BaseModel(model.BaseModel):
+    name = models.TextField()
+    class Meta:
+        abstract = True
