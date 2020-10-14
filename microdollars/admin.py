168c6adca6090dc6c234a1ffa88d34fb2dacbbbc
vinay.garimella <vg5ak@virginia.edu>
2020-10-13 21:29:29 -0400

    Added OrganizationModel

--- microdollars/admin.py
+++ microdollars/admin.py
@@ -2,3 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 #test
+from .models import OrganizationModel as O
+#import Organization Model into admin
+admin.site.register(O)
--- microdollars/models.py
+++ microdollars/models.py
@@ -8,16 +8,26 @@ from django.db import models
 #         abstract = True
 
 
+class OrganizationModel(models.Model):
+    organization_name = models.CharField(max_length=100)
+    about_me = models.TextField(max_length = 1000, default='INFO ON THIS ORGANIZATION')
+    # total_donations = ''
+    # your_donation = ''
+    def __str__(self):
+        return self.organization_name
+
+
 
 class Donation(models.Model):
-    ORGANIZATION_CHOICES= [
+    ORGANIZATION_CHOICES = [
         ('red cross', 'Red Cross'),
         ('non-profit #1', 'Non-profit #1'),
         ('non-profit #2', 'Non-profit #2'),
     ]
-    donateto = models.CharField(max_length=30, choices=ORGANIZATION_CHOICES, default='Red Cross')
+
+    donateto = models.ForeignKey(OrganizationModel, on_delete=models.SET_NULL, null=True)
     amount = models.PositiveIntegerField()
     comment = models.CharField(max_length=350)
 
-    def getOrganizationChoices(self):
-        return self.ORGANIZATION_CHOICES
+    def convertToTuple(self, info):
+        obj = info.objects.all()
