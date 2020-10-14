34856c358cbd2f1002120b8b913beb05d85a2dcf
vinay.garimella <vg5ak@virginia.edu>
2020-10-13 21:54:31 -0400

    Added Payment by user to Admin. Note: need to group payments by specific user in the future.

--- microdollars/admin.py
+++ microdollars/admin.py
@@ -3,5 +3,7 @@ from django.contrib import admin
 # Register your models here.
 #test
 from .models import OrganizationModel as O
+from .models import Donation as D
 #import Organization Model into admin
 admin.site.register(O)
+admin.site.register(D)
