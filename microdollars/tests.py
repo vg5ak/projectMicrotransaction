96047cc58046f809a6b4badc15867ea4c6f981fc
John Kim <jjk3crr@virginia.edu>
2020-10-18 21:58:10 -0400

    Partial tests

--- microdollars/tests.py
+++ microdollars/tests.py
@@ -1,18 +1,13 @@
-from _typeshed import NoneType
-from django.db.models.fields import NullBooleanField
-from microdollars.forms import DonationForm
-from microdollars.models import Donation, OrganizationModel
 from django.test import TestCase
-from django.test import TestCase
-# Create your tests here.
 
+# Create your tests here.
+from .models import  OrganizationModel, Donation
 
 class OrganizationTest(TestCase):
     #Donation.objects.create() 
     #assertEquals("RequiredOutput", This DonationModel.str())
     def create_org(self):
         return OrganizationModel.objects.create()
-
     def test_about_me(self):
         item = self.create_org()
         self.assertEquals("INFO ON THIS ORGANIZATION", item.about_me )
@@ -21,16 +16,17 @@ class OrganizationTest(TestCase):
         item = self.create_org()
         self.assertEquals(item.organization_name, "")
 
-    def test_false_tests(self):
+    def test_false_name(self):
         item = self.create_org()
-        self.assertNotEqual(item.about_me, "Anything else")
-        self.assertNotEqual(item.organization_name, "Red Cross")
+        self.assertNotEqual(item.organization_name, "Anything")
+    def test_false_about_me(self):
+        item = self.create_org()
+        self.assertNotEqual(item.about_me, "INFO ON THIS ORGANIZATIO")
 
-
-class DonationTest(TestCase):
-    def create_donation(self):
-        return Donation.objects.create()
+#class DonationTest(TestCase):
+    #def create_donate(self):
+        #return Donation.objects.create()
+    #def test_comment(self):
+        #item = self.create_donate()
+        #self.assertContains(item.comment, "")
     
-    def test_comment(self):
-        donate = self.create_donation()
-        self.assertEquals(donate.comment, "")
