012792bbcb4eb41f8e131a3f77eab773c3a62127
Vasil Zhigilei <vasil.zhigilei@gmail.com>
2020-11-12 12:47:42 -0500

    debug false

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,9 +39,9 @@ MEDIA_URL = '/media/'
 SECRET_KEY = 'eexvvh6p_h6guk2k#5q*ob_(gljpu1#o^x@b9%+skj&-e6kb6f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://cs3240-2jv.herokuapp.com']
 
 
 # Application definition
